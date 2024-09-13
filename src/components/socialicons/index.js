3a1773770392f98800a4c1fdd278cdf56453be5e
Steven Albert <steven.albert2001@gmail.com>
2024-09-13 03:20:19 +0300

    first push

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -7,6 +7,8 @@ import {
   FaLinkedin,
   FaYoutube,
   FaTwitch,
+  FaInstagram,
+  FaSoundcloud,
 } from "react-icons/fa";
 import { socialprofils } from "../../content_option";
 
@@ -14,24 +16,17 @@ export const Socialicons = (params) => {
   return (
     <div className="stick_follow_icon">
       <ul>
-        {socialprofils.twitter && (
-          <li>
-            <a href={socialprofils.twitter}>
-              <FaTwitter />
-            </a>
-          </li>
-        )}
-        {socialprofils.github && (
-          <li>
-            <a href={socialprofils.github}>
-              <FaGithub />
-            </a>
-          </li>
-        )}
-        {socialprofils.facebook && (
+      {socialprofils.facebook && (
           <li>
             <a href={socialprofils.facebook}>
               <FaFacebookF />
+            </a>
+          </li>
+        )}
+           {socialprofils.instagram && (
+          <li>
+            <a href={socialprofils.instagram}>
+              <FaInstagram />
             </a>
           </li>
         )}
@@ -42,20 +37,15 @@ export const Socialicons = (params) => {
             </a>
           </li>
         )}
-        {socialprofils.youtube && (
+        {socialprofils.soundcloud && (
           <li>
-            <a href={socialprofils.youtube}>
-              <FaYoutube />
+            <a href={socialprofils.soundcloud}>
+              <FaSoundcloud />
             </a>
           </li>
         )}
-        {socialprofils.twitch && (
-          <li>
-            <a href={socialprofils.twitch}>
-              <FaTwitch />
-            </a>
-          </li>
-        )}
+ 
+
       </ul>
       <p>Follow Me</p>
     </div>
--- src/content_option.js
+++ src/content_option.js
@@ -1,157 +1,133 @@
-const logotext = "JOHN";
+const logotext = "STEVEN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Steven Albert",
+    description: "I’m Steven Albert, a music producer and sound engineer.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Steven Albert",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I create cinematic soundscapes",
+        second: "I direct orchestral emotions",
+        third: "I craft dramatic music experiences",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "Passionate about producing cinematic and orchestral music that stirs deep emotions. With years of experience in dramatic sound design, composition, and orchestration.",
+    your_img_url: "https://firebasestorage.googleapis.com/v0/b/bell-25e54.appspot.com/o/room.jpg?alt=media&token=d9ad2284-3a6c-41b8-91fa-5000da55d028",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "About me",
+    aboutme: "A Christian, Egyptian teen who found his passion in music and tech, eager to make an impact through art and technology. I have started producing music since I was 15 years old, and alongside my music journey, I’m also a software engineer, which allows me to combine my technical skills with creativity. My love for cinematic and orchestral compositions drives me to create soundscapes that connect with people on an emotional level, while my background in software engineering gives me a unique edge in problem-solving and innovation. Music is my way of expressing the beauty I see in the world, and technology helps me bring that vision to life.",
 };
+
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Music Producer of the Year",
+        where: "Berlin Music Awards",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Sound Engineer",
+        where: "Skyline Studios",
+        date: "2021",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Freelance Music Producer",
+        where: "Various Artists",
+        date: "2018-Present",
     },
 ];
 
 const skills = [{
-        name: "Python",
+        name: "Music Production",
         value: 90,
     },
     {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
+        name: "Mixing & Mastering",
         value: 80,
     },
     {
-        name: "React",
+        name: "Sound Design",
+        value: 80,
+    },
+    {
+        name: "Video Editing",
         value: 60,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "Content Creation",
+        value: 60,
     },
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Cinematic Music Production & Film Scoring",
+        description: "From conceptualizing a track to delivering a final polished product, I specialize in producing high-quality music ready for the big screen.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Mixing & Mastering",
+        description: "I offer basic mixing and mastering services to ensure your tracks are ready for radio, streaming platforms, and more.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Sound Design",
+        description: "Creating unique sounds that enhance the emotion and experience of music, tailored to each project’s needs.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i1.sndcdn.com/avatars-000320273463-uvuz1x-t500x500.jpg",
+        description: "Peaceful War - A cinematic orchestral piece that captures the essence of a battle for peace.",
+        link: "https://soundcloud.com/user-14270029/peaceful-war?si=671bdceb69aa48d98b4b0c6269ea8359&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://firebasestorage.googleapis.com/v0/b/bell-25e54.appspot.com/o/dina.png?alt=media&token=3f13e820-9530-42e2-bc36-9276b7765a12",
+        description: "Main Theme for Youth of the bible - an oriental emotional piece that captures contradicting feelings. ",
+        link: "https://1drv.ms/u/s!AtQNv7l1cClmgpoonX3E5l5H5ovFOQ?e=lddiWa",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://firebasestorage.googleapis.com/v0/b/bell-25e54.appspot.com/o/Screenshot%202024-09-13%20022527.png?alt=media&token=03a23579-c78e-43ed-99b4-821df59f5e38",
+        description: "A suit and a Dress - A short film where music is the main character.",
+        link: "https://www.youtube.com/watch?v=pRK3tNm5jIE",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://firebasestorage.googleapis.com/v0/b/bell-25e54.appspot.com/o/Screenshot%202024-09-13%20015740.png?alt=media&token=b1d83bc2-6a13-492e-bb17-aa97910a2e9c",
+        description: "Youth of the bible - A mini series telling stories from the bible in a modern way. I composed the entire music for the series.",
+        link: "https://www.youtube.com/watch?v=cMbOll_Rf9U&list=PLfMybXXJ12t3WI4pX3qLAJ71Xx6hR5T7w&index=2",
     },
+  
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://firebasestorage.googleapis.com/v0/b/bell-25e54.appspot.com/o/desert.jpg?alt=media&token=3902cae5-74b7-4735-9934-c1d67dc4bbbd",
+        description: "Oriental Dialogue - A fusion of traditional and modern music, blending Eastern and Western influences.",
+        link: "https://soundcloud.com/user-14270029/oriental-dialogue?si=57d32692f55c4a6e80e58e2730aa3f24&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://firebasestorage.googleapis.com/v0/b/bell-25e54.appspot.com/o/SOLOMOMN2.jpg?alt=media&token=f88fbe82-b783-490b-978e-51aa2db54c0a",
+        description: "Melancholy - A melancholic piece that takes you on a rollercoaster of emotions.",
+        link: "https://1drv.ms/u/s!AtQNv7l1cClmgdAd7pFowce3XFTpqg?e=GdNDQd",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://firebasestorage.googleapis.com/v0/b/bell-25e54.appspot.com/o/download.webp?alt=media&token=35809179-aaec-44e6-98fc-629afb3ae304",
+        description: "Ressurection - An emotional piece that tells the story of how Jesus saved us all, leading us from darkness to light.",
+        link: "https://1drv.ms/u/s!AtQNv7l1cClmgdAg0EI8rF3erQEpOg?e=oKaAXa",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_EMAIL: "steven.albert2001@gmail.com",
+    YOUR_FONE: "(+20)127 602 272 9",
+    description: "Feel free to get in touch for collaborations, music production inquiries, or any sound design projects.",
+    YOUR_SERVICE_ID: "service_tmuxaj8", // Replace with EmailJS service ID
+    YOUR_TEMPLATE_ID: "template_qj8nnjq", // Replace with EmailJS template ID
+    YOUR_USER_ID: "dF3lWOUbUSC1JqQNT", // Replace with EmailJS user ID
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    facebook: "https://www.facebook.com/steven.albert2001",
+    linkedin: "https://www.linkedin.com/in/steven-albert-511933231/",
+    soundcloud: "https://soundcloud.com/user-14270029",
+    instagram: "https://www.instagram.com/steevenalbert/",
 };
+
 export {
     meta,
     dataabout,
